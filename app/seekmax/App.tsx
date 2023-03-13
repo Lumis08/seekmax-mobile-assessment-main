7c8c2a76817b4259c2eaca71cdcb4345501bba48
william.lim <william.lim@hexainitiative.com>
2023-03-13 14:35:51 +0800

    [APP] Cleanup app

--- app/seekmax/App.tsx
+++ app/seekmax/App.tsx
@@ -1,35 +1,11 @@
 import React from 'react';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
-import {setContext} from '@apollo/client/link/context';
+import {ApolloProvider} from '@apollo/client';
+import apolloClient from './src/graphql/apollo-client';
 import RootNavigator from './src/navigation/RootNavigator';
-
-// GraphQL
-const httpLink = createHttpLink({uri: 'http://192.168.1.107:3002'});
-const authLink = setContext((_, {headers}) => {
-  // get the authentication token from local storage if it exists
-  // const token = localStorage.getItem('token');
-  // return the headers to the context so httpLink can read them
-  let token = null;
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
 
 function App(): JSX.Element {
   return (
-    <ApolloProvider client={client}>
+    <ApolloProvider client={apolloClient}>
       <RootNavigator />
     </ApolloProvider>
   );
